6a8d2382ac9dc232e2f56e503416e0a841d9160f
方雄磊 <122764498+fxlqwq@users.noreply.github.com>
2024-05-11 19:12:47 +0800

    Update is.sh

--- is.sh
+++ is.sh
@@ -1,8 +1,10 @@
-
 #!/bin/bash
 
 # Set default action (install package maintainer's version)
 SSHD_CONFIG_ACTION="install"
+
+# Set non-interactive environment for apt-get
+export DEBIAN_FRONTEND=noninteractive
 
 # Check if environment variable is set to keep local version
 if [ "$SSHD_CONFIG_ACTION" == "keep" ]; then
