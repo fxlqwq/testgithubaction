76ded5c37df8c7e663ece33484ca8ca095ca3911
方雄磊 <122764498+fxlqwq@users.noreply.github.com>
2024-05-11 20:09:09 +0800

    Update is.sh

--- is.sh
+++ is.sh
@@ -1,16 +1,6 @@
-#!/bin/bash
-
-# Set default action (install package maintainer's version)
-SSHD_CONFIG_ACTION="install"
-
-# Set non-interactive environment for apt
-export DEBIAN_FRONTEND=noninteractive
-
-# Check if environment variable is set to keep local version
-if [ "$SSHD_CONFIG_ACTION" == "keep" ]; then
-    echo "Keeping local version of sshd_config."
-else
-    # Otherwise, install package maintainer's version
-    echo "Installing package maintainer's version of sshd_config."
-    sudo apt-get install --reinstall -o Dpkg::Options::="--force-confold" openssh-server -y
-fi
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok -y
