36f72f7833018a35d61c2ff363fb99bf4699a268
方雄磊 <122764498+fxlqwq@users.noreply.github.com>
2024-05-11 19:10:53 +0800

    Update is.sh

--- is.sh
+++ is.sh
@@ -10,5 +10,5 @@ if [ "$SSHD_CONFIG_ACTION" == "keep" ]; then
 else
     # Otherwise, install package maintainer's version
     echo "Installing package maintainer's version of sshd_config."
-    sudo apt-get install --reinstall -o Dpkg::Options::="--force-confnew" openssh-server
+    sudo apt-get install --reinstall -o Dpkg::Options::="--force-confnew" openssh-server -y
 fi
