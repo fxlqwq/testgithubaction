bb35329dafacbc7dd40ca9c3ed68183de85a8968
方雄磊 <122764498+fxlqwq@users.noreply.github.com>
2024-05-11 19:09:09 +0800

    Update is.sh

--- is.sh
+++ is.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+
+# Set default action (install package maintainer's version)
+SSHD_CONFIG_ACTION="install"
+
+# Check if environment variable is set to keep local version
+if [ "$SSHD_CONFIG_ACTION" == "keep" ]; then
+    echo "Keeping local version of sshd_config."
+else
+    # Otherwise, install package maintainer's version
+    echo "Installing package maintainer's version of sshd_config."
+    sudo apt-get install --reinstall -o Dpkg::Options::="--force-confnew" openssh-server
+fi
